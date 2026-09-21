a6dceae7a5d51a08f0de05e501ca50c6d7f5f636
AlbBris <alban.brissez@gmail.com>
2026-09-21 09:38:45 +0200

    Add files via upload

---
+++ js/seed-data.js
@@ -0,0 +1,19 @@
+window.POINTAGES_SEED = {
+  "version": 1.14,
+  "project": {
+    "id": "nouveau-chantier",
+    "name": "Nouveau chantier",
+    "code": "",
+    "management": "",
+    "defaultZone": ""
+  },
+  "currentWeek": {
+    "year": 2026,
+    "week": 32
+  },
+  "people": [],
+  "tasks": [],
+  "teams": [],
+  "defaultSchedules": {"GCC": [8, 8, 8, 8, 7], "Interim": [7.5, 7.5, 7.5, 7.5, 7], "PretMO": [7.5, 7.5, 7.5, 7.5, 7]},
+  "weeks": {}
+};
